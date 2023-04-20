8861adc68963c144da235c9f1e1a4399ba9871b2
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-04-20 20:15:17 +0200

    added README and database file to import the table

---
+++ Database/se_project.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 20, 2023 at 07:59 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `se_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car_selling_posts`
+--
+
+CREATE TABLE `car_selling_posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `car_selling_posts`
+--
+
+INSERT INTO `car_selling_posts` (`id`, `title`, `description`, `price`, `image_url`, `created_at`, `user_id`) VALUES
+(9, 'BMW 325i (E30) Wagon', 'A desirable classic BMW sports wagon from BMW', 200000, 'images/uploads/Background.png', '2023-04-20 04:16:15', 1),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `PhoneNumber` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `created_at`, `PhoneNumber`) VALUES
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `car_selling_posts`
+--
+ALTER TABLE `car_selling_posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `email_2` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `car_selling_posts`
+--
+ALTER TABLE `car_selling_posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `car_selling_posts`
+--
+ALTER TABLE `car_selling_posts`
+  ADD CONSTRAINT `car_selling_posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,42 @@
+SoukAuto
+
+This is a school project for the Software Engineering course. The project is a car selling website called "SoukAuto" that allows users to post their cars for sale and browse the available listings. The website was built using HTML, CSS, JavaScript, and PHP.
+
+Getting Started
+
+To get started with the project, clone the repository using the following command:
+
+git clone https://github.com/imadChr/SoukAuto.git
+
+Prerequisites :
+
+To run the project, you will need to have XAMPP or a similar local server installed on your computer.
+
+Installing :
+
+Install XAMPP or a similar local server.
+Clone the repository to your local machine.
+Copy the "SoukAuto" folder to the "htdocs" folder in your XAMPP installation directory.
+Start the Apache and MySQL services in XAMPP.
+Import the "se_project.sql" file located in the "Database" folder into your MySQL server using phpMyAdmin or a similar tool.
+
+Usage :
+
+To use the website, open a web browser and go to the following URL:
+
+http://localhost/SoukAuto/Home/Pages/index.php
+
+This will take you to the homepage of the website where you can browse the available car listings.
+To post a car for sale, click on the "Sell" button and fill out the form.
+To view the details of a specific car listing, click on the title of the listing.
+
+Authors :
+
+Kadri Mohammed Mouncef - WebDev Developer and Back-End developer
+CHARCHAR Imad Eddine - Front-end developer
+Yousfi Chaker - Design and Front-end developer
+GRINE Abdessamed Bassel - Front-end contributor
+
+Acknowledgments :
+The project was completed as part of the Software Engineering course at The National School Of Artificial Intelligence.
+Special thanks to Mr.BOUCHRIKA and Mme.CHETTAH for providing guidance and support throughout the project.
