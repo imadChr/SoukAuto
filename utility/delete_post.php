c6841b7bd00c2778c13d3912be4a73067c99fe35
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-04-29 17:04:10 +0200

    fixed delete function for posts

--- utility/delete_post.php
+++ utility/delete_post.php
@@ -6,7 +6,7 @@ require_once 'functions.php';
 if (isset($_SESSION["user_id"])) {
     $user_id = $_SESSION["user_id"];
     $post_id = $_GET["post_id"];
-    if (deletePost($conn, $user_id, $post_id)) {
+    if (deletePost($user_id, $post_id)) {
         $_SESSION["message"] = "Post deleted successfully";
         header("Location: ../index.php");
         exit();
