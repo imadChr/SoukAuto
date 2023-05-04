89bb65fc4ba5a6c746dfd58aeb93485eca7a4228
RahilRahM <rahil.rahmouni@ensia.edu.dz>
2023-05-04 14:44:54 +0100

    minor updates

--- Pages/all_posts.php
+++ Pages/all_posts.php
@@ -18,9 +18,15 @@ require_once '../utility/db_connection.php';
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
+        crossorigin="anonymous">    
     <!-- Iconbox -->
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+        <link
+        href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+        rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" 
+        rel="stylesheet" />
     <!-- style css -->
     <link rel="stylesheet" href="../css/all_posts.css">
 </head>
@@ -30,7 +36,7 @@ require_once '../utility/db_connection.php';
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container px-4 px-lg-5">
-            <img><a class="navbar-brand" href="#!">[logo]</a>
+                <img><a class="navbar-brand" href="../index.php">[logo]</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
@@ -40,9 +46,7 @@ require_once '../utility/db_connection.php';
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#!">All Products</a></li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
+                                <li><hr class="dropdown-divider" /></li>
                             <li><a class="dropdown-item" href="#!">Popular Items</a></li>
                             <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                         </ul>
@@ -68,21 +72,23 @@ require_once '../utility/db_connection.php';
         </div>
     </header>
     <!--shopping part-->
-    <div class="container">
+        <div class="container cards_landscape_wrap-2">
         <div class="row">
             <!--prod 1-->
             <div class="col-md-6 col-lg-3">
-                <div class="card mb-4">
+                    <div class="card mb-4"> <!--TRY: card-flyer --> 
                     <!-- RENT/SELL badge-->
                     <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">RENT</div>
                     <!-- Product image-->
-                    <img class="card-img-top" src="../images/car.png" alt="Card image cap">
+                        <img class="card-img-top " src="../images/car.png" alt="Card image cap">
                     <!--card body-->
                     <div class="card-body">
-                        <!-- Product name-->
+                            <!-- Product details-->
                         <h5 class="card-title">[year] [car name]</h5>
                         <p class="card-text">[car description]</p>
-                        <p class="card-text"><small class="text-muted">[date of post]</small></p>
+                            <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+                            <!--icons-->
+                            
                     </div>
                 </div>
             </div>
@@ -121,10 +127,11 @@ require_once '../utility/db_connection.php';
                     <img class="card-img-top" src="../images/mazda.png" alt="Card image cap">
                     <!--card body-->
                     <div class="card-body">
-                        <!-- Product name-->
+                            <!-- Product details-->
                         <h5 class="card-title">[year] [car name]</h5>
                         <p class="card-text">[car description]</p>
-                        <p class="card-text"><small class="text-muted">[date of post]</small></p>
+                            <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+
                     </div>
                 </div>
             </div>
@@ -137,10 +144,11 @@ require_once '../utility/db_connection.php';
                     <img class="card-img-top" src="../images/car.png" alt="Card image cap">
                     <!--card body-->
                     <div class="card-body">
-                        <!-- Product name-->
+                            <!-- Product details-->
                         <h5 class="card-title">[year] [car name]</h5>
                         <p class="card-text">[car description]</p>
-                        <p class="card-text"><small class="text-muted">[date of post]</small></p>
+                            <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+
                     </div>
                 </div>
             </div>
@@ -153,10 +161,11 @@ require_once '../utility/db_connection.php';
                     <img class="card-img-top" src="../images/mazda.png" alt="Card image cap">
                     <!--card body-->
                     <div class="card-body">
-                        <!-- Product name-->
+                            <!-- Product details-->
                         <h5 class="card-title">[year] [car name]</h5>
                         <p class="card-text">[car description]</p>
-                        <p class="card-text"><small class="text-muted">[date of post]</small></p>
+                            <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+
                     </div>
                 </div>
             </div>
@@ -164,6 +173,28 @@ require_once '../utility/db_connection.php';
 
         </div>
     </div>
+        <!-- Pagination -->
+        <nav aria-label="Page navigation example" class="d-flex justify-content-center mt-3">
+            <ul class="pagination">
+            <li class="page-item disabled">
+                <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">4</a></li>
+            <li class="page-item"><a class="page-link" href="#">5</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+            </ul>
+        </nav>
+        <!-- Bootstrap JavaScript files -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
