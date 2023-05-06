4ea0701a539044431144b1d1e588cea754f9c180
RahilRahM <rahil.rahmouni@ensia.edu.dz>
2023-05-06 16:57:54 +0100

    all_posts final (filter/pagination/toggler button)

--- Pages/all_posts.php
+++ Pages/all_posts.php
@@ -1,6 +1,6 @@
 <?php
+session_start();
 require_once '../utility/db_connection.php';
-require_once '../utility/functions.php';
 
 // set the number of posts per page
 $posts_per_page = 9;
@@ -40,11 +40,26 @@ $total_pages = ceil($total_posts / $posts_per_page);
     <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
-        crossorigin="anonymous">    
+        crossorigin="anonymous">  
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">  
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/css/datepicker.min.css" 
+        rel="stylesheet">
     <!-- Iconbox -->
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <link
+        href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+        rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" 
+        rel="stylesheet" />
+        <script src="https://use.fontawesome.com/72b693e5a2.js"></script>
+        <!-- date picker -->
+        <script src="
+        https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js
+        "></script>
+        <link href="
+        https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css
+        " rel="stylesheet">
     <!-- style css -->
     <link rel="stylesheet" href="../css/all_posts.css">
 </head>
@@ -57,7 +72,7 @@ $total_pages = ceil($total_posts / $posts_per_page);
                 <img src="../images/logo.png" class="logo"><a class="navbar-brand" href="../index.php"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -69,18 +84,19 @@ $total_pages = ceil($total_posts / $posts_per_page);
                             <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                         </ul>
                     </li>
-                </ul>
-                <form class="d-flex">
+                    </ul> -->
+                <form class="d-flex ms-auto">
                     <button class="btn btn-outline-dark" type="submit">
                         <i class="bi-cart-fill me-1"></i>
-                        favorites
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            favorites
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span> <!--add number of cars in favorite-->
                     </button>
                 </form>
             </div>
         </div>
     </nav>
-    <!-- Header-->
+
+        <!-- Header -->
     <header class="bg-dark py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
@@ -89,113 +105,202 @@ $total_pages = ceil($total_posts / $posts_per_page);
             </div>
         </div>
     </header>
-    <!--filter-->
-    <div class="input-group input-daterange btn-sm">
-  <select class="form-control" id="yearFrom">
-    <option value="">From year</option>
-    <!-- Add options for year range here -->
-    <?php
-    $currentYear = date('Y');
-    for ($i = $currentYear; $i >= 1900; $i--) {
-      echo "<option value='$i'>$i</option>";
-    }
-    ?>
-  </select>
-  <div class="input-group-addon">to</div>
-  <select class="form-control" id="yearTo">
-    <option value="">To year</option>
-    <!-- Add options for year range here -->
-    <?php
-    $currentYear = date('Y');
-    for ($i = $currentYear; $i >= 1900; $i--) {
-      echo "<option value='$i'>$i</option>";
-    }
-    ?>
-  </select>
-</div>
-    <!--shopping part-->
-    <div class="container cards_landscape_wrap-2"> //class pb-5 
+
+        <!--Filter nav-->
+    <div class="container">
         <div class="row">
-            <!--product-->
-            <?php
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-            ?>
+              <div class="col-md-12">
+
+                <!-- Filter Toggle Button -->
+                <div class="d-flex justify-content my-3 ml-3">
+                  <button type="button" class="btn btn-outline-primary toggle-filter-btn">
+                    <ion-icon name="funnel-outline"></ion-icon>
+                  </button>
+                </div>
+
+                <!-- Filter Form -->
+                <form class="filter-form d-none">
+                    <div class="row">
+                        <div class="col-md-4 mb-3">
+                            <!-- Brand -->
+                            <label for="brand">Brand:</label>
+                            <select class="form-control" id="brand">
+                                <option>Toyota</option>
+                                <option>Honda</option>
+                                <option>Nissan</option>
+                                <option>Hyundai</option>
+                                <option>Kia</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <!-- Wilaya -->
+                            <label for="wilaya">Wilaya:</label>
+                            <select class="form-control" id="wilaya">
+                                <option>Algiers</option>
+                                <option>Oran</option>
+                                <option>Constantine</option>
+                                <option>Batna</option>
+                                <option>Setif</option>
+                            </select>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <!-- Country -->
+                            <label for="country">Country:</label>
+                            <select class="form-control" id="country">
+                                <option>Japan</option>
+                                <option>Korea</option>
+                                <option>Germany</option>
+                                <option>USA</option>
+                                <option>France</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- Price range -->
+                        <div class="col-md-4 mb-3">
+                            <label for="price ">Price:</label>
+                            <input type="range" class="form-range" id="price" step="100000"  min="1000000" max="10000000">
+                            <div class="price-range-values"></div>
+                            <div class="d-flex justify-content-between">
+                                <span>1,000,000</span>
+                                <span>10,000,000</span>
+                            </div>
+                            <input type="range" class="form-range" id="price2" step="100000"  min="1000000" max="10000000">
+                        </div>
+                        <!-- Mileage Range -->
+                        <div class="col-md-4 mb-3">
+                            <label for="mileage">Mileage:</label>
+                            <input type="range" class="form-range" id="mileage" step="5000" min="0" max="500000">
+                            <div class="d-flex justify-content-between">
+                                <span>0</span>
+                                <span>500,000</span>
+                            </div>
+                            <input type="range" class="form-range" id="mileage2" step="5000" min="0" max="500000">
+                        </div>
+                        <!-- year Range -->
+                        <div class="col-md-4 mb-3">
+                            <label for="year">Year:</label>
+                            <input type="text" class="form-control datepicker-year" class="year" name="year" placeholder="From" min="1970" max="2023">
+                            <br>
+                            <input type="text" class="form-control datepicker-year" class="year" name="year" placeholder="To" min="1970" max="2023">
+                        </div>
+                    </div>
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Filter</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+        <!--shopping part-->
+        <div class="container cards_landscape_wrap-2 pb-5">
+            <div class="row">
+                <!--prod 1-->
                     <div class="col-md-6 col-lg-3">
+                    <div class="container">
                         <div class="card mb-4">
                             <!-- RENT/SELL badge-->
-                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sell</div>
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">RENT</div>
                             <!-- Product image-->
-                            <img class="card-img-top" src="../<?php echo $row['image_url']; ?>" alt="Card image cap">
+                            <img class="card-img-top " src="../images/car.png" alt="Card image cap">
                             <!--card body-->
                             <div class="card-body">
-                                <!-- Product name-->
-                                <h5 class="card-title"><?php echo $row['year'], $row['title'] ?></h5>
-                                <p class="card-text"><?php echo $row['description'] ?></p>
-                                <p class="card-text"><?php echo $row['price'] ?> DA</p>
+                                <!-- Product details-->
+                                <h3 class="card-title">[year] [car name]</h3>
+                                <h5 style="color:#ED6C15;">[price] DA</h5>
+                                <p class="card-text">[car description]</p>
+                                <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+                                <!--icons-->
+                                <button class="e-button btn-sm expand-btn" role="button">
+                                    <span class="e-button-text"><ion-icon name="heart-outline"></ion-icon> Add To Favorites</span> 
+                                </button>
+                                  
+                                <button class="e-button btn-sm expand-btn" role="button">
+                                    <span class="e-button-text"><ion-icon name="person-outline"></ion-icon> Contact Seller</span> 
+                                </button>
+                                  
+                                <button class="e-button btn-sm expand-btn" role="button">
+                                    <span class="e-button-text"><ion-icon name="add-outline"></ion-icon> Show more</span> 
+                                </button>
                             </div>
                         </div>
                     </div>
-            <?php
-                }
+                </div>
+                <!-- Add more cards here -->
+
+            </div>
+        </div>
+            <!-- Pagination -->
+        <div class="row mb-5">
+            <div class="col-md-4"></div> <!-- empty column on the left -->
+            <div class="col-md-4 text-center"> <!-- center column for pagination -->
+                <h2 class="heading-section"></h2>
+                <div class="block-27">
+                    <ul>
+                        <li><a href="#">&lt;</a></li>
+                        <li class="active"><span>1</span></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">&gt;</a></li>
+                    </ul>
+            </div>
+            </div>
+            <div class="col-md-4"></div> <!-- empty column on the right -->
+        </div>
+
+
+        
+        <!-- Bootstrap JavaScript files -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="https://netdna.bootstrapcdn.com/bootstrap/2.3.2/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.2.0/js/bootstrap-datepicker.min.js"></script>
+        <script>
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip()
+            })
+
+
+            //item in favorite list (change later)
+            const button = document.querySelector('.e-button');
+            let isFavorite = false;
+
+            button.addEventListener('click', () => {
+            const icon = button.querySelector('ion-icon');
+            isFavorite = !isFavorite;
+            if (isFavorite) {
+                icon.setAttribute('name', 'heart-dislike-outline');
+            } else {
+                icon.setAttribute('name', 'heart-outline');
             }
-            ?>
-            <!--prod 5-->
-            <div class="col-md-6 col-lg-3">
-                <div class="card mb-4">
-                    <!-- RENT/SELL badge-->
-                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">SALE</div>
-                    <!-- Product image-->
-                    <img class="card-img-top" src="../images/mazda.png" alt="Card image cap">
-                    <!--card body-->
-                    <div class="card-body">
-                        <!-- Product details-->
-                        <h5 class="card-title">[year] [car name]</h5>
-                        <p class="card-text">[car description]</p>
-                        <p class="card-text"><small class="text-muted">[date of post] , [wilaya] </small></p>
+            });
 
-                        <!-- Bootstrap JS -->
-                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-nmW8uO7vmJ2fB6C9j6U3bIggEoGJ4oAXKj0p0zJL+RRbiRj75h42M9XSDP+oOksM" crossorigin="anonymous"></script>
+            //togle filter button
+            $(document).ready(function(){
+                $(".toggle-filter-btn").click(function(){
+                $(".filter-form").toggleClass("d-none");
+                });
+            });
+            
+            //date picker
+            $(function() {
+            $('.datepicker-year').datepicker({
+                format: 'yyyy',
+                viewMode: 'years',
+                minViewMode: 'years',
+                startDate: '1970',
+                endDate: '2023'
+            });
+            
+            $('#year').on('changeDate', function() {
+                const year = $(this).datepicker('getDate').getFullYear();
+                console.log(year);
+            });
+            });
+        </script> 
 
-                    </div>
-                </div>
-                <!-- Pagination -->
-                <div class="row mb-5">
-                    <div class="col-md-4"></div> <!-- empty column on the left -->
-                    <div class="col-md-4 text-center"> <!-- center column for pagination -->
-                        <h2 class="heading-section"></h2>
-                        <div class="block-27">
-                            <ul class="pagination justify-content-center">
-                                <li><a href="#">&lt;</a></li>
-                                <?php
-                                // Define variables for the number of total pages and the current page
-                                $total_pages = 5;
-                                $current_page = 1;
-
-                                // Generate the pagination links using a loop
-                                for ($i = 1; $i <= $total_pages; $i++) {
-                                    if ($i == $current_page) {
-                                        echo "<li class='active'><span>" . $i . "</span></li>";
-                                    } else {
-                                        echo "<li><a href='#'>" . $i . "</a></li>";
-                                    }
-                                }
-                                ?>
-                                <li><a href="#">&gt;</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md-4"></div> <!-- empty column on the right -->
-                </div>
-
-
-                <!-- Bootstrap JavaScript files -->
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-                <script>
-                    $(function () {
-                        $('[data-toggle="tooltip"]').tooltip()
-                    })
-                </script>    
-</body>
-
+    </body>
 </html>
