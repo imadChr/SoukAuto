5112919d300c1d4e6e0e0f65e8381aa2015b33bf
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-05-27 18:26:12 +0200

    test

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -16,51 +16,6 @@ $(document).ready(function() {
         $(".filter-form").toggleClass("d-none");
     });
 
-    // Add to favorites
-    function addToFavorites(post_id) {
-        // Send an AJAX request to the server to add or remove the post from the user's favorites
-        $.ajax({
-            url: '../utility/functions.php?action=addtofavorites',
-            method: 'post',
-            data: {
-                post_id: post_id
-            },
-            success: function(response) {
-                // Update the UI to reflect the changes
-                if (response == 'added') {
-                    // The post was added to the user's favorites
-                    alert('Post added to favorites');
-                    const icon = document.querySelector('.e-button ion-icon');
-                    icon.setAttribute('name', 'heart-dislike-outline');
-                    icon.classList.add('favorite');
-                } else if (response == 'removed') {
-                    // The post was removed from the user's favorites
-                    alert('Post removed from favorites');
-                    const icon = document.querySelector('.e-button ion-icon');
-                    icon.setAttribute('name', 'heart-outline');
-                    icon.classList.remove('favorite');
-                }
-            },
-            error: function() {
-                alert('An error occurred');
-            }
-        });
-    }
-
-    const button = document.querySelector('.e-button');
-    let isFavorite = false;
-
-    button.addEventListener('click', () => {
-        const icon = button.querySelector('ion-icon');
-        isFavorite = !isFavorite;
-        if (isFavorite) {
-            addToFavorites(post_id);
-            icon.setAttribute('name', 'heart-dislike-outline');
-        } else {
-            icon.setAttribute('name', 'heart-outline');
-        }
-    });
-
     // Pagination
         $(document).on('click', '.page-link', function(e) {
             e.preventDefault();
