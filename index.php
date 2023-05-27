4d875a1087f4f794b41b038133ed9848b47cf3af
MoonFuji <mohammed.kadri@ensia.edu.dz>
2023-05-27 13:55:04 +0200

    fixed the redirect

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ $public_actions = array('do_login', 'signup', 'do_signup');
 
 //non-logged user are forced to the login page...
 if ($appuser == 0  and !in_array($vars['action'], $public_actions)) {
-	$vars['action'] = 'home';
+  $vars['action'] = 'login';
 } else if (is_array($appuser) and !isset($vars['action'])) {
-	$vars['action'] = 'home';
+  $vars['action'] = 'home';
 }
 
 include("modules/user.php");
 //Modules to be accessed only by logged users...
 if (is_array($appuser)) {
-	include("modules/home.php");
-	include("modules/post.php");
+  include("modules/home.php");
+  include("modules/post.php");
 }
